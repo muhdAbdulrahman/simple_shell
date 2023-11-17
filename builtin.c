be1fe9e18e632168329fd19606ecf327c04eeb12
muhdAbdulrahman <132393678+muhdAbdulrahman@users.noreply.github.com>
2023-11-17 22:54:16 +0100

    Update builtin.c

--- builtin.c
+++ builtin.c
@@ -1,93 +1,93 @@
 #include "simpleshell.h"
 
 /**
- * _myexit - Exit the shell.
- * @info: Struct containing potential args.
- * Return: Exits with status 0 if info.argv[0] != "exit".
- */
+* _myexit - Exit the shell.
+* @info: Struct containing potential args.
+* Return: Exits with status 0 if info.argv[0] != "exit".
+*/
 int _myexit(info_t *info)
 {
-	int exit_check;
+int exit_check;
 
-	if (info->argv[1])
-	{
-		exit_check = _erratoi(info->argv[1]);
-		if (exit_check == -1)
-		{
-			info->status = 2;
-			print_error(info, "Illegal number: ");
-			_eputs(info->argv[1]);
-			_eputchar('\n');
-			return (1);
-		}
-		info->err_num = _erratoi(info->argv[1]);
-		return (-2);
-	}
-	info->err_num = -1;
-	return (-2);
+if (info->argv[1])
+{
+exit_check = _erratoi(info->argv[1]);
+if (exit_check == -1)
+{
+info->status = 2;
+print_error(info, "Illegal number: ");
+_eputs(info->argv[1]);
+_eputchar('\n');
+return (1);
+}
+info->err_num = _erratoi(info->argv[1]);
+return (-2);
+}
+info->err_num = -1;
+return (-2);
 }
 
 /**
- * _mycd - Change the current directory of the process.
- * @info: Struct containing potential args.
- * Return: Always returns 0.
- */
+* _mycd - Change the current directory of the process.
+* @info: Struct containing potential args.
+* Return: Always returns 0.
+*/
 int _mycd(info_t *info)
 {
-	char *current_dir, *new_dir, buffer[1024];
-	int chdir_ret;
+char *current_dir, *new_dir, buffer[1024];
+int chdir_ret;
 
-	current_dir = getcwd(buffer, 1024);
-	if (!current_dir)
-		_puts("TODO: >>getcwd failure emsg here<<\n");
-	if (!info->argv[1])
-	{
-		new_dir = _getenv(info, "HOME=");
-		if (!new_dir)
-			chdir_ret = /* TODO: what should this be? */
-				chdir((new_dir = _getenv(info, "PWD=")) ? new_dir : "/");
-		else
-			chdir_ret = chdir(new_dir);
-	}
-	else if (_strcmp(info->argv[1], "-") == 0)
-	{
-		if (!_getenv(info, "OLDPWD="))
-		{
-			_puts(current_dir);
-			_putchar('\n');
-			return (1);
-		}
-		_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
-		chdir_ret = /* TODO: what should this be? */
-			chdir((new_dir = _getenv(info, "OLDPWD=")) ? new_dir : "/");
-	}
-	else
-		chdir_ret = chdir(info->argv[1]);
-	if (chdir_ret == -1)
-	{
-		print_error(info, "can't cd to ");
-		_eputs(info->argv[1]), _eputchar('\n');
-	}
-	else
-	{
-		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
-		_setenv(info, "PWD", getcwd(buffer, 1024));
-	}
-	return (0);
+current_dir = getcwd(buffer, 1024);
+if (!current_dir)
+_puts("TODO: >>getcwd failure emsg here<<\n");
+if (!info->argv[1])
+{
+new_dir = _getenv(info, "HOME=");
+if (!new_dir)
+chdir_ret = /* TODO: what should this be? */
+chdir((new_dir = _getenv(info, "PWD=")) ? new_dir : "/");
+else
+chdir_ret = chdir(new_dir);
+}
+else if (_strcmp(info->argv[1], "-") == 0)
+{
+if (!_getenv(info, "OLDPWD="))
+{
+_puts(current_dir);
+_putchar('\n');
+return (1);
+}
+_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
+chdir_ret = /* TODO: what should this be? */
+chdir((new_dir = _getenv(info, "OLDPWD=")) ? new_dir : "/");
+}
+else
+chdir_ret = chdir(info->argv[1]);
+if (chdir_ret == -1)
+{
+print_error(info, "can't cd to ");
+_eputs(info->argv[1]), _eputchar('\n');
+}
+else
+{
+_setenv(info, "OLDPWD", _getenv(info, "PWD="));
+_setenv(info, "PWD", getcwd(buffer, 1024));
+}
+return (0);
 }
 
 /**
- * _myhelp - Display help information (Not yet implemented).
- * @info: Struct containing potential args.
- * Return: Always returns 0.
- */
+* _myhelp - Display help information (Not yet implemented).
+* @info: Struct containing potential args.
+* Return: Always returns 0.
+*/
 int _myhelp(info_t *info)
 {
-	char **arg_array;
+char **arg_array;
 
-	arg_array = info->argv;
-	_puts("help call works. Function not yet implemented \n");
-	if (0)
-		_puts(*arg_array); /* temp att_unused workaround */
-	return (0);
+arg_array = info->argv;
+_puts("help call works. Function not yet implemented \n");
+if (0)
+_puts(*arg_array); /* temp att_unused workaround */
+return (0);
 }
