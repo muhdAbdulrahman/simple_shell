fff4bfcfba5d7a494d617a7e5e2144e94148d3b0
muhdAbdulrahman <132393678+muhdAbdulrahman@users.noreply.github.com>
2023-11-17 23:00:59 +0100

    Update parser.c

--- parser.c
+++ parser.c
@@ -1,86 +1,86 @@
 #include "simpleshell.h"
 
 /**
- * is_cmd - Function to check if a file is an executable command.
- * @info: Parameter info struct.
- * @path: File path.
- *
- * Return: 1 if true, otherwise return 0.
- */
+* is_cmd - Function to check if a file is an executable command.
+* @info: Parameter info struct.
+* @path: File path.
+*
+* Return: 1 if true, otherwise return 0.
+*/
 int is_cmd(info_t *info, char *path)
 {
-	struct stat st;
+struct stat st;
 
-	(void)info;
-	if (!path || stat(path, &st))
-		return (0);
+(void)info;
+if (!path || stat(path, &st))
+return (0);
 
-	if (st.st_mode & S_IFREG)
-	{
-		return (1);
-	}
-	return (0);
+if (st.st_mode & S_IFREG)
+{
+return (1);
+}
+return (0);
 }
 
 /**
- * dup_chars - Method that duplicates characters.
- * @pathstr: String path.
- * @start: The start index.
- * @stop: The end index.
- *
- * Return: Pointer to the new buffer.
- */
+* dup_chars - Method that duplicates characters.
+* @pathstr: String path.
+* @start: The start index.
+* @stop: The end index.
+*
+* Return: Pointer to the new buffer.
+*/
 char *dup_chars(char *pathstr, int start, int stop)
 {
-	static char buf[1024];
-	int i = 0, k = 0;
+static char buf[1024];
+int i = 0, k = 0;
 
-	for (k = 0, i = start; i < stop; i++)
-		if (pathstr[i] != ':')
-			buf[k++] = pathstr[i];
-	buf[k] = 0;
-	return (buf);
+for (k = 0, i = start; i < stop; i++)
+if (pathstr[i] != ':')
+buf[k++] = pathstr[i];
+buf[k] = 0;
+return (buf);
 }
 
 /**
- * find_path - Search for a command in the PATH string.
- * @info: Information struct.
- * @pathstr: String representing the PATH.
- * @cmd: The command to find.
- *
- * Return: Full path of the command if found, or NULL if not found.
- */
+* find_path - Search for a command in the PATH string.
+* @info: Information struct.
+* @pathstr: String representing the PATH.
+* @cmd: The command to find.
+*
+* Return: Full path of the command if found, or NULL if not found.
+*/
 char *find_path(info_t *info, char *pathstr, char *cmd)
 {
-	int i = 0, curr_pos = 0;
-	char *path;
+int i = 0, curr_pos = 0;
+char *path;
 
-	if (!pathstr)
-		return (NULL);
-	if ((_strlen(cmd) > 2) && starts_with(cmd, "./"))
-	{
-		if (is_cmd(info, cmd))
-			return (cmd);
-	}
-	while (1)
-	{
-		if (!pathstr[i] || pathstr[i] == ':')
-		{
-			path = dup_chars(pathstr, curr_pos, i);
-			if (!*path)
-				_strcat(path, cmd);
-			else
-			{
-				_strcat(path, "/");
-				_strcat(path, cmd);
-			}
-			if (is_cmd(info, path))
-				return (path);
-			if (!pathstr[i])
-				break;
-			curr_pos = i;
-		}
-		i++;
-	}
-	return (NULL);
+if (!pathstr)
+return (NULL);
+if ((_strlen(cmd) > 2) && starts_with(cmd, "./"))
+{
+if (is_cmd(info, cmd))
+return (cmd);
+}
+while (1)
+{
+if (!pathstr[i] || pathstr[i] == ':')
+{
+path = dup_chars(pathstr, curr_pos, i);
+if (!*path)
+_strcat(path, cmd);
+else
+{
+_strcat(path, "/");
+_strcat(path, cmd);
+}
+if (is_cmd(info, path))
+return (path);
+if (!pathstr[i])
+break;
+curr_pos = i;
+}
+i++;
+}
+return (NULL);
 }
