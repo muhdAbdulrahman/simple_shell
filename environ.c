8d131fb4e0d66f0e05d8d9ff73c892618476f1e1
muhdAbdulrahman <132393678+muhdAbdulrahman@users.noreply.github.com>
2023-11-17 22:55:08 +0100

    Update environ.c

--- environ.c
+++ environ.c
@@ -1,90 +1,90 @@
 #include "simpleshell.h"
 
 /**
- * _myenv - Print current environment variables.
- * @info: Struct containing potential args.
- * Return: Always return 0.
- */
+* _myenv - Print current environment variables.
+* @info: Struct containing potential args.
+* Return: Always return 0.
+*/
 int _myenv(info_t *info)
 {
-	print_list_str(info->env);
-	return (0);
+print_list_str(info->env);
+return (0);
 }
 
 /**
- * _getenv - Get the value of an environment variable.
- * @info: Struct containing potential args.
- * @name: Environment variable name.
- * Return: Return the value or NULL if not found.
- */
+* _getenv - Get the value of an environment variable.
+* @info: Struct containing potential args.
+* @name: Environment variable name.
+* Return: Return the value or NULL if not found.
+*/
 char *_getenv(info_t *info, const char *name)
 {
-	list_t *node = info->env;
-	char *value;
+list_t *node = info->env;
+char *value;
 
-	while (node)
-	{
-		value = starts_with(node->str, name);
-		if (value && *value)
-			return (value);
-		node = node->next;
-	}
-	return (NULL);
+while (node)
+{
+value = starts_with(node->str, name);
+if (value && *value)
+return (value);
+node = node->next;
+}
+return (NULL);
 }
 
 /**
- * _mysetenv - Initialize a new environment variable or modify the present one.
- * @info: Struct containing potential args.
- * Return: Return 0 on success, 1 on incorrect number of arguments.
- */
+* _mysetenv - Initialize a new environment variable or modify the present one.
+* @info: Struct containing potential args.
+* Return: Return 0 on success, 1 on incorrect number of arguments.
+*/
 int _mysetenv(info_t *info)
 {
-	if (info->argc != 3)
-	{
-		_eputs("Incorrect number of arguments\n");
-		return (1);
-	}
+if (info->argc != 3)
+{
+_eputs("Incorrect number of arguments\n");
+return (1);
+}
 
-	if (_setenv(info, info->argv[1], info->argv[2]))
-		return (0);
+if (_setenv(info, info->argv[1], info->argv[2]))
+return (0);
 
-	return (1);
+return (1);
 }
 
 /**
- * _myunsetenv - Remove an environment variable.
- * @info: Struct containing potential args.
- * Return: Always return 0.
- */
+* _myunsetenv - Remove an environment variable.
+* @info: Struct containing potential args.
+* Return: Always return 0.
+*/
 int _myunsetenv(info_t *info)
 {
-	int i;
+int i;
 
-	if (info->argc == 1)
-	{
-		_eputs("Too few arguments.\n");
-		return (1);
-	}
+if (info->argc == 1)
+{
+_eputs("Too few arguments.\n");
+return (1);
+}
 
-	for (i = 1; i <= info->argc; i++)
-		_unsetenv(info, info->argv[i]);
+for (i = 1; i <= info->argc; i++)
+_unsetenv(info, info->argv[i]);
 
-	return (0);
+return (0);
 }
 
 /**
- * populate_env_list - Populate environment linked list.
- * @info: Struct containing potential args.
- * Return: Always return 0.
- */
+* populate_env_list - Populate environment linked list.
+* @info: Struct containing potential args.
+* Return: Always return 0.
+*/
 int populate_env_list(info_t *info)
 {
-	list_t *node = NULL;
-	size_t i;
+list_t *node = NULL;
+size_t i;
 
-	for (i = 0; environ[i]; i++)
-		add_node_end(&node, environ[i], 0);
+for (i = 0; environ[i]; i++)
+add_node_end(&node, environ[i], 0);
 
-	info->env = node;
-	return (0);
+info->env = node;
+return (0);
 }
