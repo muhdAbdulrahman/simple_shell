9b4e7a30e507112534893c6a79a11fbf418f0b11
muhdAbdulrahman <132393678+muhdAbdulrahman@users.noreply.github.com>
2023-11-17 22:14:15 +0100

    Update lists.c

--- lists.c
+++ lists.c
@@ -1,158 +1,174 @@
 #include "simpleshell.h"
 
 /**
- * add_node - fxn to add a node to start of the list
- * @head: address of pointer to head node
- * @str: node string
- * @num: node index used by history
+ * add_node - Function to add a node to the start of the list.
+ * @head: Address of pointer to head node.
+ * @str: Node string.
+ * @num: Node index used by history.
  *
- * Return: return the size of list
+ * Return: Pointer to the newly added node.
  */
 list_t *add_node(list_t **head, const char *str, int num)
 {
-	list_t *new_head;
+    list_t *new_node;
 
-	if (!head)
-		return (NULL);
-	new_head = malloc(sizeof(list_t));
-	if (!new_head)
-		return (NULL);
-	_memset((void *)new_head, 0, sizeof(list_t));
-	new_head->num = num;
-	if (str)
-	{
-		new_head->str = _strdup(str);
-		if (!new_head->str)
-		{
-			free(new_head);
-			return (NULL);
-		}
-	}
-	new_head->next = *head;
-	*head = new_head;
-	return (new_head);
+    if (!head)
+        return (NULL);
+
+    new_node = malloc(sizeof(list_t));
+    if (!new_node)
+        return (NULL);
+
+    _memset((void *)new_node, 0, sizeof(list_t));
+    new_node->num = num;
+
+    if (str)
+    {
+        new_node->str = _strdup(str);
+        if (!new_node->str)
+        {
+            free(new_node);
+            return (NULL);
+        }
+    }
+
+    new_node->next = *head;
+    *head = new_node;
+    return (new_node);
 }
 
 /**
- * add_node_end - fxn to include node to the end of list
- * @head: address of pointer to head node
- * @str: node string
- * @num: node index used by history
- * Return: return size of list
+ * add_node_end - Function to add a node to the end of the list.
+ * @head: Address of pointer to head node.
+ * @str: Node string.
+ * @num: Node index used by history.
+ *
+ * Return: Pointer to the newly added node.
  */
 list_t *add_node_end(list_t **head, const char *str, int num)
 {
-	list_t *new_node, *node;
+    list_t *new_node, *node;
 
-	if (!head)
-		return (NULL);
+    if (!head)
+        return (NULL);
 
-	node = *head;
-	new_node = malloc(sizeof(list_t));
-	if (!new_node)
-		return (NULL);
-	_memset((void *)new_node, 0, sizeof(list_t));
-	new_node->num = num;
-	if (str)
-	{
-		new_node->str = _strdup(str);
-		if (!new_node->str)
-		{
-			free(new_node);
-			return (NULL);
-		}
-	}
-	if (node)
-	{
-		while (node->next)
-			node = node->next;
-		node->next = new_node;
-	}
-	else
-		*head = new_node;
-	return (new_node);
+    node = *head;
+    new_node = malloc(sizeof(list_t));
+    if (!new_node)
+        return (NULL);
+
+    _memset((void *)new_node, 0, sizeof(list_t));
+    new_node->num = num;
+
+    if (str)
+    {
+        new_node->str = _strdup(str);
+        if (!new_node->str)
+        {
+            free(new_node);
+            return (NULL);
+        }
+    }
+
+    if (node)
+    {
+        while (node->next)
+            node = node->next;
+        node->next = new_node;
+    }
+    else
+    {
+        *head = new_node;
+    }
+
+    return (new_node);
 }
 
 /**
- * print_list_str - this prints str element of a list_t
- * @f: pointer to first node
+ * print_list_str - Function to print the str element of a list_t.
+ * @head: Pointer to the first node.
  *
- * Return: return size of list
+ * Return: Size of the list.
  */
-size_t print_list_str(const list_t *f)
+size_t print_list_str(const list_t *head)
 {
-	size_t i = 0;
+    size_t count = 0;
 
-	while (f)
-	{
-		_puts(f->str ? f->str : "(nil)");
-		_puts("\n");
-		f = f->next;
-		i++;
-	}
-	return (i);
+    while (head)
+    {
+        _puts(head->str ? head->str : "(nil)");
+        _puts("\n");
+        head = head->next;
+        count++;
+    }
+
+    return (count);
 }
 
 /**
- * delete_node_at_index - fxn that will delete node at an index
- * @head: pointer to first node
- * @index: index of node to delete
+ * delete_node_at_index - Function to delete a node at a specific index.
+ * @head: Pointer to pointer to the first node.
+ * @index: Index of the node to delete.
  *
- * Return: return 1 on success, return 0 if void
+ * Return: 1 on success, 0 if void.
  */
 int delete_node_at_index(list_t **head, unsigned int index)
 {
-	list_t *node, *prev_node;
-	unsigned int i = 0;
+    list_t *node, *prev_node;
+    unsigned int i = 0;
 
-	if (!head || !*head)
-		return (0);
+    if (!head || !*head)
+        return (0);
 
-	if (!index)
-	{
-		node = *head;
-		*head = (*head)->next;
-		free(node->str);
-		free(node);
-		return (1);
-	}
-	node = *head;
-	while (node)
-	{
-		if (i == index)
-		{
-			prev_node->next = node->next;
-			free(node->str);
-			free(node);
-			return (1);
-		}
-		i++;
-		prev_node = node;
-		node = node->next;
-	}
-	return (0);
+    if (!index)
+    {
+        node = *head;
+        *head = (*head)->next;
+        free(node->str);
+        free(node);
+        return (1);
+    }
+
+    node = *head;
+    while (node)
+    {
+        if (i == index)
+        {
+            prev_node->next = node->next;
+            free(node->str);
+            free(node);
+            return (1);
+        }
+        i++;
+        prev_node = node;
+        node = node->next;
+    }
+
+    return (0);
 }
 
 /**
- * free_list - fxn that free nodes of a list
- * @head_ptr: pointer to head node
+ * free_list - Function to free nodes of a list.
+ * @head_ptr: Pointer to pointer to head node.
  *
- * Return: return void
+ * Return: Void.
  */
 void free_list(list_t **head_ptr)
 {
-	list_t *node, *next_node, *head;
+    list_t *node, *next_node, *head;
 
-	if (!head_ptr || !*head_ptr)
-		return;
-	head = *head_ptr;
-	node = head;
-	while (node)
-	{
-		next_node = node->next;
-		free(node->str);
-		free(node);
-		node = next_node;
-	}
-	*head_ptr = NULL;
+    if (!head_ptr || !*head_ptr)
+        return;
+
+    head = *head_ptr;
+    node = head;
+    while (node)
+    {
+        next_node = node->next;
+        free(node->str);
+        free(node);
+        node = next_node;
+    }
+
+    *head_ptr = NULL;
 }
