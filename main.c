f155c75c37d2c6b3b7bdf6f8e6b9df0e5ea758b1
muhdAbdulrahman <132393678+muhdAbdulrahman@users.noreply.github.com>
2023-11-17 23:00:16 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,44 +1,44 @@
 #include "simpleshell.h"
 
 /**
- * main - The entry point.
- * @argc: Argument count.
- * @argv: Argument vector.
- *
- * Return: 0 on success, 1 if void.
- */
+* main - The entry point.
+* @argc: Argument count.
+* @argv: Argument vector.
+*
+* Return: 0 on success, 1 if void.
+*/
 int main(int argc, char **argv)
 {
-	info_t info[] = {INFO_INIT};
-	int file_descriptor = 2;
+info_t info[] = {INFO_INIT};
+int file_descriptor = 2;
 
-	asm ("mov %1, %0\n\t"
-			"add $3, %0"
-			: "=r" (file_descriptor)
-			: "r" (file_descriptor));
+asm ("mov %1, %0\n\t"
+"add $3, %0"
+: "=r" (file_descriptor)
+: "r" (file_descriptor));
 
-	if (argc == 2)
-	{
-		file_descriptor = open(argv[1], O_RDONLY);
-		if (file_descriptor == -1)
-		{
-			if (errno == EACCES)
-				exit(126);
-			if (errno == ENOENT)
-			{
-				_eputs(argv[0]);
-				_eputs(": 0: Can't open ");
-				_eputs(argv[1]);
-				_eputchar('\n');
-				_eputchar(BUF_FLUSH);
-				exit(127);
-			}
-			return (EXIT_FAILURE);
-		}
-		info->readfd = file_descriptor;
-	}
-	populate_env_list(info);
-	read_history(info);
-	hsh(info, argv);
-	return (EXIT_SUCCESS);
+if (argc == 2)
+{
+file_descriptor = open(argv[1], O_RDONLY);
+if (file_descriptor == -1)
+{
+if (errno == EACCES)
+exit(126);
+if (errno == ENOENT)
+{
+_eputs(argv[0]);
+_eputs(": 0: Can't open ");
+_eputs(argv[1]);
+_eputchar('\n');
+_eputchar(BUF_FLUSH);
+exit(127);
+}
+return (EXIT_FAILURE);
+}
+info->readfd = file_descriptor;
+}
+populate_env_list(info);
+read_history(info);
+hsh(info, argv);
+return (EXIT_SUCCESS);
 }
