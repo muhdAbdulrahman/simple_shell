bf01373d0b8033d057e2088b52de8df833351a81
muhdAbdulrahman <132393678+muhdAbdulrahman@users.noreply.github.com>
2023-11-17 23:00:37 +0100

    Update memory.c

--- memory.c
+++ memory.c
@@ -1,18 +1,18 @@
 #include "simpleshell.h"
 
 /**
- * bfree - fxn that NULLs the address and free pointer
- * @ptr: pointer address
- *
- * Return: Return 1 if freed, Return 0 if otherwise
- */
+* bfree - fxn that NULLs the address and free pointer
+* @ptr: pointer address
+*
+* Return: Return 1 if freed, Return 0 if otherwise
+*/
 int bfree(void **ptr)
 {
-	if (ptr && *ptr)
-	{
-		free(*ptr);
-		*ptr = NULL;
-		return (1);
-	}
-	return (0);
+if (ptr && *ptr)
+{
+free(*ptr);
+*ptr = NULL;
+return (1);
+}
+return (0);
 }
