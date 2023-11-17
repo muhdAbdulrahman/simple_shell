7393f012e87d3da7e177b3cb64972833eced278e
muhdAbdulrahman <132393678+muhdAbdulrahman@users.noreply.github.com>
2023-11-17 17:40:12 +0100

    Update atoi.c

--- atoi.c
+++ atoi.c
@@ -1,74 +1,66 @@
 #include "simpleshell.h"
 
 /**
- * interactive - return true if shell is in interactive mode
- * @info: struct address
+ * Check if the shell is in interactive mode.
  *
- * Return: return 1 if interactive mode, 0 if otherwise
+ * @param info The struct containing shell information.
+ * @return 1 if in interactive mode, 0 otherwise.
  */
-int interactive(info_t *info)
-{
-	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+int interactive(info_t *info) {
+    return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
 
 /**
- * is_delim - Checks whether char is a delimeter
- * @chr: the char to check
- * @delim: this is the delimeter string
- * Return: return 1 if true, 0 if otherwise
+ * Check whether a character is a delimiter.
+ *
+ * @param chr The character to check.
+ * @param delim The delimiter string.
+ * @return 1 if true, 0 otherwise.
  */
-int is_delim(char chr, char *delim)
-{
-	while (*delim)
-		if (*delim++ == chr)
-			return (1);
-	return (0);
+int is_delim(char chr, char *delim) {
+    while (*delim) {
+        if (*delim++ == chr)
+            return 1;
+    }
+    return 0;
 }
 
 /**
- * _isalpha - this will check for alphabetic character
- * @chr: The char to input
- * Return: Return 1 if c is alphabetic, otherwise return 0
+ * Check if a character is alphabetic.
+ *
+ * @param chr The character to check.
+ * @return 1 if alphabetic, 0 otherwise.
  */
-
-int _isalpha(int chr)
-{
-	if ((chr >= 'a' && chr <= 'z') || (chr >= 'A' && chr <= 'Z'))
-		return (1);
-	else
-		return (0);
+int _isalpha(int chr) {
+    return ((chr >= 'a' && chr <= 'z') || (chr >= 'A' && chr <= 'Z'));
 }
 
 /**
- * _atoi - function  will convert the chr string to an integer
- * @strg: the string to be converted
- * Return: return 0 if no numbers in string, converted number otherwise
+ * Convert a string to an integer.
+ *
+ * @param strg The string to be converted.
+ * @return 0 if no numbers in the string, the converted number otherwise.
  */
+int _atoi(char *strg) {
+    int i, sign = 1, flag = 0, output;
+    unsigned int result = 0;
 
-int _atoi(char *strg)
-{
-	int i, sign = 1, flag = 0, output;
-	unsigned int result = 0;
+    for (i = 0; strg[i] != '\0' && flag != 2; i++) {
+        if (strg[i] == '-')
+            sign *= -1;
 
-	for (i = 0; strg[i] != '\0' && flag != 2; i++)
-	{
-		if (strg[i] == '-')
-			sign *= -1;
+        if (strg[i] >= '0' && strg[i] <= '9') {
+            flag = 1;
+            result *= 10;
+            result += (strg[i] - '0');
+        } else if (flag == 1)
+            flag = 2;
+    }
 
-		if (strg[i] >= '0' && strg[i] <= '9')
-		{
-			flag = 1;
-			result *= 10;
-			result += (strg[i] - '0');
-		}
-		else if (flag == 1)
-			flag = 2;
-	}
+    if (sign == -1)
+        output = -result;
+    else
+        output = result;
 
-	if (sign == -1)
-		output = -result;
-	else
-		output = result;
-
-	return (output);
+    return output;
 }
